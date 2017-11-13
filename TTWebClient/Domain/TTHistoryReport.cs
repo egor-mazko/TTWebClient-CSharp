149f321ebf16a5530592407408f5d255308d9bfc
Ivan Shynkarenka <chronoxor@gmail.com>
2017-11-13 15:47:21 +0300

    Add SrcAsset/DstAsset movements to TTTradeHistory

--- TTWebClient/Domain/TTHistoryReport.cs
+++ TTWebClient/Domain/TTHistoryReport.cs
@@ -15,7 +15,8 @@ namespace TTWebClient.Domain
         PositionClosed  = 4,
         Balance         = 5,
         Credit          = 6,
-        PositionOpened  = 7
+        PositionOpened  = 7,
+        OrderActivated  = 8
     } 
     
     /// <summary>
@@ -30,7 +31,8 @@ namespace TTWebClient.Domain
         TakeProfitAct   = 4,
         DealerDecision  = 5,
         Rollover        = 6,
-        Delete          = 7
+        Delete          = 7,
+        Expired         = 8
     }
 
     /// <summary>
@@ -52,6 +54,9 @@ namespace TTWebClient.Domain
 
         /// <summary>Symbol</summary>
         public string Symbol { get; set; }
+
+        /// <summary>Symbol precision</summary>
+        public int? SymbolPrecision { get; set; }
 
         /// <summary>Trade Id</summary>
         public long TradeId { get; set; }
@@ -77,6 +82,12 @@ namespace TTWebClient.Domain
         /// <summary>Trade amount</summary>
         public decimal? TradeAmount { get; set; }
 
+        /// <summary>Trade max visible amount</summary>
+        public decimal? TradeMaxVisibleAmount { get; set; }
+
+        /// <summary>Trade hidden amount</summary>
+        public decimal? TradeHiddenAmount { get; set; }
+
         /// <summary>Trade initial amount</summary>
         public decimal? TradeInitialAmount { get; set; }
 
@@ -95,8 +106,23 @@ namespace TTWebClient.Domain
         /// <summary>Request timestamp</summary>
         public DateTime? RequestTimestamp { get; set; }
 
+        /// <summary>Request open price</summary>
+        public decimal? RequestOpenPrice { get; set; }
+
+        /// <summary>Request open amount</summary>
+        public decimal? RequestOpenAmount { get; set; }
+
+        /// <summary>Request close price</summary>
+        public decimal? RequestClosePrice { get; set; }
+
+        /// <summary>Request close amount</summary>
+        public decimal? RequestCloseAmount { get; set; }
+
         /// <summary>Position Id</summary>
         public long? PositionId { get; set; }
+
+        /// <summary>Position by Id</summary>
+        public long? PositionById { get; set; }
 
         /// <summary>Position amount</summary>
         public decimal? PositionAmount { get; set; }
@@ -140,11 +166,32 @@ namespace TTWebClient.Domain
         /// <summary>Agent commission</summary>
         public decimal? AgentCommission { get; set; }
 
+        /// <summary>Reduced Open Commission Flag</summary>
+        public bool? ReducedOpenCommissionFlag { get; set; }
+
+        /// <summary>Reduced Close Commission Flag</summary>
+        public bool? ReducedCloseCommissionFlag { get; set; }
+
+        /// <summary>Minimal Commission Currency</summary>
+        public string MinCommissionCurrency { get; set; }
+
+        /// <summary>Minimal Commission Conversion Rate</summary>
+        public decimal? MinCommissionConversionRate { get; set; }
+
         /// <summary>Swap</summary>
         public decimal? Swap { get; set; }
 
+        /// <summary>Immediate-Or-Cancel flag</summary>
+        public bool ImmediateOrCancel { get; set; }
+
+        /// <summary>Market-With-Slippage flag</summary>
+        public bool MarketWithSlippage { get; set; }
+
         /// <summary>Expired timestamp</summary>
         public DateTime? Expired { get; set; }
+
+        /// <summary>Time of described position modification.</summary>
+        public DateTime? PosModified { get; set; }
 
         /// <summary>Comment</summary>
         public string Comment { get; set; }
@@ -157,6 +204,69 @@ namespace TTWebClient.Domain
 
         /// <summary>Close conversion rate</summary>
         public decimal? CloseConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert profit to USD</summary>
+        public decimal? ProfitToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to profit currency</summary>
+        public decimal? UsdToProfitConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert balance currency to USD</summary>
+        public decimal? BalanceToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to balance currency</summary>
+        public decimal? UsdToBalanceConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert margin currency to USD</summary>
+        public decimal? MarginCurrencyToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to margin currency</summary>
+        public decimal? UsdToMarginCurrencyConversionRate { get; set; }
+
+        /// <summary>Margin currency</summary>
+        public string MarginCurrency { get; set; }
+
+        /// <summary>Conversion rate to convert profit currency to USD</summary>
+        public decimal? ProfitCurrencyToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to profit currency</summary>
+        public decimal? UsdToProfitCurrencyConversionRate { get; set; }
+
+        /// <summary>Profit currency</summary>
+        public string ProfitCurrency { get; set; }
+
+        /// <summary>Conversion rate to convert source asset currency to USD</summary>
+        public decimal? SrcAssetToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to source asset currency</summary>
+        public decimal? UsdToSrcAssetConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert destination asset currency to USD</summary>
+        public decimal? DstAssetToUsdConversionRate { get; set; }
+
+        /// <summary>Conversion rate to convert USD to destination asset currency</summary>
+        public decimal? UsdToDstAssetConversionRate { get; set; }
+
+        /// <summary>Source asset currency</summary>
+        public string SrcAssetCurrency { get; set; }
+
+        /// <summary>Source asset final amount</summary>
+        public decimal? SrcAssetAmount { get; set; }
+
+        /// <summary>Amount on which source asset was changed during described transaction</summary>
+        public decimal? SrcAssetMovement { get; set; }
+
+        /// <summary>Destination asset currency</summary>
+        public string DstAssetCurrency { get; set; }
+
+        /// <summary>Destination asset final amount</summary>
+        public decimal? DstAssetAmount { get; set; }
+
+        /// <summary>Amount on which destinaction asset was changed during described transaction</summary>
+        public decimal? DstAssetMovement { get; set; }
+
+        /// <summary>Client appliction</summary>
+        public string ClientApp { get; set; }
     }
 
     /// <summary>
